72ea62f5a90c80bd548050475c6e935e6bb1feab
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-04-14 09:30:48 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    currentAccount(4425, "kishoiyan", 300.00)
+
+}
+class currentAccount(var number: Int, var name: String, var balance:Int){
+    fun deposit(amount:Double):Int{
+        var y= deposit+ balance
+        var deposit=
+
+
+    }
+    fun withdraw(amount: Double):Int{
+
+    }
+    fun details(){
+
+    }
+}
---
+++ src/main/kotlin/new.kt
@@ -0,0 +1,49 @@
+import javax.print.attribute.standard.MediaSize.Other
+
+fun main() {
+    currentAccount(4425, "anna",  400)
+    var y=("joannah")
+
+
+}
+open class currentAccount( number: Int,  name: String, balance:Int) {
+
+    fun deposit(amount: Double): Double {
+        var r = amount + deposit(88.00)
+        println(r)
+
+
+        fun withdraw(amount:Double){
+        var y=amount-withdraw(98.00)
+        println(y)
+
+    }
+
+   fun details(){
+       println("the account number 4425 with balance 400 is operated by anna")
+
+   }
+
+}
+open class savingAccount(var number: Int, var name: String, var balance:Int){
+    fun withdrawal(cash:Int){
+
+
+    }
+}
+
+}
+class product(name:String, price:Int, category:Other){
+    fun category
+}
+
+
+fun all(names:String){
+    for (name in names){
+        println(name.even)
+
+    }
+    return names
+
+}
+
